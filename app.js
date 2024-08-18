337f73c9e5d4705a9303fd66f16fb07db8aa2a08
Elimes Rodrgíuez <elimes29@gmail.com>
2024-08-18 19:34:11 -0400

    Modifica app.js con ejercicio opcional

--- app.js
+++ app.js
@@ -3,3 +3,13 @@ const edad = prompt('¿Cuántos años tienes?');
 const lenguaje = prompt('¿Qué lenguaje de programación estás estudiando?');
 
 alert(`Hola ${nombre}, tienes ${edad} años y ya estás aprendiendo ${lenguaje}!`);
+
+const band = prompt(`¿Te gusta estudiar ${lenguaje}? Responde con el número 1 para SÍ o 2 para NO.`);
+
+if (band== 1) {
+    alert('¡Muy bien! Sigue estudiando y tendrás mucho éxito.');
+} else if (band == 2) {
+    alert('Oh, qué pena... ¿Ya intentaste aprender otros lenguajes?');
+} else {
+    alert(`ERROR: Debias introducir "1" o "2" e introduciste ${band}`);
+}
